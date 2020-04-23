1fb828c8a716fd8105c76bb30c4e859035271df9
Likhesh Mahajan <likhesh@Mac-Pro.local>
2020-04-23 15:49:17 +1000

    package upgrades

---
+++ src/components/sign-up/SignUp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import FormInput from "../form-input/FormInput";
+import CustomButton from "../custom-button/CustomButton";
+
+import "./signup.styles.scss";
+
+const SignUp = () => {
+  return <div></div>;
+};
+
+export default SignUp;
