f96bd13293d9b408225024d80f87f2411a2c6904
Likhesh Mahajan <likhesh@Mac-Pro.local>
2020-04-23 14:06:25 +1000

    signin sign up page and signin and custom button component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/ShopPage";
 import Header from "./components/header/Header";
+import SignInSignUp from "./pages/sign-in-sign-up/SignInSignUp";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInSignUp} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-sign-up/SignInSignUp.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SignIn from "../../components/SignIn/SignIn";
+
+import "./signinsignup.styles.scss";
+
+const SignInSignUp = () => {
+  return (
+    <div className="sign-in-sign-up">
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInSignUp;
